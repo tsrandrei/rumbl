5c4f7324c772646e2c952a7b1f3f5fd8791a27d6
TsrAndrei <4539062+tsrandrei@users.noreply.github.com>
2020-05-15 17:03:18 +0200

    Updates

--- test/rumbl/accounts_test.exs
+++ test/rumbl/accounts_test.exs
@@ -13,7 +13,10 @@ defmodule Rumbl.AccountsTest do
     @invalid_attrs %{}
 
     test "with valid data inserts user" do
-      
+      assert {:ok, %User{id: id}=user} = Accounts.register_user(@valid_attrs)
+      assert user.name == "Joe"
+      assert user.username == "joe"
+      assert [%User{id: ^id}] = Accounts.list_users()
     end
   end
 end
